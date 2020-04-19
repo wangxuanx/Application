cd86d3e2058a14947a2e546e72b9f3eb40a85216
wangxuan <1484642072@qq.com>
2020-04-19 13:44:26 +0800

    加入群组功能实现

--- app/src/main/java/com/example/application/ui/home/AddGroupActivity.java
+++ app/src/main/java/com/example/application/ui/home/AddGroupActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.application.R;
+import com.example.application.http.HttpsUtil;
+import com.example.application.http.SharedPrefUtil;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddGroupActivity extends AppCompatActivity {
+    /**加入群组*/
     private CircleImageView circleImageView;
     private TextView nameText;
     private TextView descripeText;
@@ -28,11 +32,14 @@ public class AddGroupActivity extends AppCompatActivity {
         init();
 
         nameText.setText(title);
+        String userName = SharedPrefUtil.getUserName(this);        //用户名
 
         button.setOnClickListener(new View.OnClickListener() {             //点击按钮添加群组
             @Override
             public void onClick(View view) {
-                AddGroup(title);
+                System.out.println(title);
+
+                AddGroup(title, userName);
             }
         });
     }
@@ -44,7 +51,35 @@ public class AddGroupActivity extends AppCompatActivity {
         button = findViewById(R.id.add_group_button);
     }
 
-    private void AddGroup(String title){          //向服务器发送信息添加入本群组
-        String url = "https://"+title;
+    private void AddGroup(String title, String userName){          //向服务器发送信息添加入本群组
+        String url = "https://120.26.172.16:8443/AndroidTest/GroupAddUser?groupName="+title+"&addUser="+userName;
+        System.out.println(url);
+
+        HttpsUtil.getInstance().get(url, new HttpsUtil.OnRequestCallBack() {
+            @Override
+            public void onSuccess(String s) {
+                System.out.println(s);
+                if(s.equals("add user success")){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddGroupActivity.this, "加入群组成功！", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddGroupActivity.this, "加入群组失败！", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFail(Exception e) {
+
+            }
+        });
     }
 }
