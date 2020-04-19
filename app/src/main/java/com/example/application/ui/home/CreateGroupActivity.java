f5eaa95a9411e21452315ca090d36169f4d9a505
wangxuan <1484642072@qq.com>
2020-04-19 10:57:53 +0800

    在线创建群组功能实现

--- app/src/main/java/com/example/application/ui/home/CreateGroupActivity.java
+++ app/src/main/java/com/example/application/ui/home/CreateGroupActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.application.R;
@@ -15,6 +16,7 @@ import com.example.application.http.SharedPrefUtil;
 public class CreateGroupActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +27,54 @@ public class CreateGroupActivity extends AppCompatActivity {
 
         init();
 
-        String groupName = editText.getText().toString().trim();        //获取建立的群组名称
         String userName = SharedPrefUtil.getUserName(this);
 
         button.setOnClickListener(new View.OnClickListener() {          //创建群组
             @Override
             public void onClick(View view) {
-                String url = "https://120.26.172.16:8443/AndroidTest/CreatGroup?groupName="+groupName+"&creatUser="+userName;
-                HttpsUtil.getInstance().get(url, new HttpsUtil.OnRequestCallBack() {
-                    @Override
-                    public void onSuccess(String s) {
-                        System.out.println(s);
-                        if(s.equals("success register")){        //群组创建成功
+                String groupName = editText.getText().toString().trim();        //获取建立的群组名称
 
-                        } else if(s.equals("group already exist!")){         //群组名已经存在
-                            Toast.makeText(CreateGroupActivity.this, "该群组已经存在！！！", Toast.LENGTH_SHORT).show();
+                System.out.println(groupName);
+
+                if(groupName.equals("")){
+                    textView.setText("请输入群组名！！");
+                } else {
+                    textView.setText("");
+                    String url = "https://120.26.172.16:8443/AndroidTest/CreatGroup?groupName="+groupName+"&creatUser="+userName;
+                    System.out.println(url);
+                    HttpsUtil.getInstance().get(url, new HttpsUtil.OnRequestCallBack() {
+                        @Override
+                        public void onSuccess(String s) {
+                            System.out.println(s);
+                            if(s.equals("success register")){        //群组创建成功
+                                System.out.println("群组创建成功！！");
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        textView.setText("群组创建成功！！");
+                                    }
+                                });
+                                finish();
+                            } else {         //群组名已经存在
+                                System.out.println("该群组名已经存在！！");
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        textView.setText("该群组名已经存在！！");
+                                    }
+                                });
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFail(Exception e) {
+                        @Override
+                        public void onFail(Exception e) {
+                            textView.setText("创建失败！请重试！");
+                        }
+                    });
+                }
 
-                    }
-                });
             }
         });
     }
@@ -55,5 +82,6 @@ public class CreateGroupActivity extends AppCompatActivity {
     private void init(){
         editText = findViewById(R.id.create_group_name);
         button = findViewById(R.id.create_group_button);
+        textView = findViewById(R.id.create_group_info);
     }
 }
